ed545f21fb8a9b40bf4ddcffae04fc1fd3d1bdbd
Łukasz Mikołajczyk <lukasz.mikolajczyk.programming@gmail.com>
2022-09-17 20:37:38 +0200

    Initial version of Galon to Liter conversion program.

---
+++ GalToLitBase/src/GalToLitBase.java
@@ -0,0 +1,16 @@
+// This program converts gallons to liters and vice-versa
+public class GalToLitBase {
+
+	public static void main(String[] args) {
+		double gallons;
+		double liters;
+		
+		gallons = 10;
+		
+		liters = gallons * 3.7854;
+		
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+
+	}
+
+}
